420e70049810de01ced6e549ca76dc7bbea98c8a
= <=>
2024-02-23 02:42:10 +0900

    public 디렉토리 이미지 테스트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <div style={{ width: '100dvw', height: '100dvh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: `url('src/assets/bg.jpeg')`, backgroundSize: 'cover' }}>
-      <img src={'src/assets/salmon.png'} style={{ width: 200, marginBottom: 100 }} />
+      <img src={'./salmon.png'} style={{ width: 200, marginBottom: 100 }} />
       <button style={{ fontSize: 24, color: '#FFFFFF', fontWeight: '700', cursor: 'pointer' }} onClick={() => navigate('/main')}>Game Start</button>
     </div>
   );
