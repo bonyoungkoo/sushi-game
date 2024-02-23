2a333def0bcabd62570b9e2af7c4ba9dec1172c2
= <=>
2024-02-23 18:00:54 +0900

    Mute 기능 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,18 +2,24 @@ import { useEffect, useRef, useCallback, useState } from 'react';
 import * as Matter from 'matter-js';
 import { SUSHI } from '../constants/sushi'
 import { useNavigate } from 'react-router-dom';
+import { GoMute } from "react-icons/go";
+import { GoUnmute } from "react-icons/go";
 
 const Main = () => {
   const [isStart, setIsStart] = useState(false);
+  const [isMute, setIsMute] = useState(true);
+  const [score, setScore] = useState(0);
   const navigate = useNavigate();
   const canvasRef = useRef(null);
   const countRef = useRef(0);
   const indexRef = useRef(0);
+  const muteRef = useRef(true);
   const collisionIndexRef = useRef(0);
   const isDropping = useRef(false); 
   const [index, setIndex] = useState(0);
   const [collisionIndex, setCollisionIndex] = useState(0);
   const [maxIndex, setMaxIndex] = useState(0);
+  const baseURL = import.meta.env.VITE_BASE_URL;
 
   useEffect(() => {
     if (!isStart) {
@@ -95,15 +101,17 @@ const Main = () => {
             index: collision.bodyA.index + 1,
             render: {
               sprite: { 
-                texture: `./${newSushi.name}.png`,
+                texture: `${baseURL}/${newSushi.name}.png`,
                 xScale: newSushi.xScale,
                 yScale: newSushi.yScale
               }
             },
             restitution: 0.2
           });
-          const audio = new Audio('./collision.mp3');
-          audio.play();
+          if (!muteRef.current) {
+            const audio = new Audio(`${baseURL}/collision.mp3`);
+            audio.play();
+          }
           World.add(world, body);
           setCollisionIndex(collision.bodyA.index + 1);
         }
@@ -132,6 +140,11 @@ const Main = () => {
   }, [isStart]);
 
   useEffect(() => {
+    muteRef.current = isMute;
+  }, [isMute]);
+
+  useEffect(() => {
+    setScore(prev => prev + (5*collisionIndex));
     if (maxIndex > collisionIndex) {
       return;
     }
@@ -139,13 +152,15 @@ const Main = () => {
   }, [collisionIndex]);
 
   const dropSushi = useCallback((World, world, Bodies, position) => {
-    const audio = new Audio('./drop.mp3');
-    audio.play();
+    if (!muteRef.current) {
+      const audio = new Audio(`${baseURL}/drop.mp3`);
+      audio.play();
+    }
     const body = Bodies.circle(position, 100, SUSHI[indexRef.current].radius, {
       index: indexRef.current,
       render: {
         sprite: { 
-          texture: `./${SUSHI[indexRef.current].name}.png`,
+          texture: `${baseURL}/${SUSHI[indexRef.current].name}.png`,
           xScale: SUSHI[indexRef.current].xScale,
           yScale: SUSHI[indexRef.current].yScale
         }
@@ -155,7 +170,7 @@ const Main = () => {
     World.add(world, body);
     countRef.current = countRef.current + 1;
     changeIndex();
-  }, []);
+  }, [isMute]);
 
   const changeIndex = useCallback(() => {
     const nextIndex = Math.floor(Math.random() * (countRef.current < 1 ? 0 : 5));
@@ -164,26 +179,35 @@ const Main = () => {
   }, []);
 
   return (
-    <div style={{ width: '100dvw', height: '100dvh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: `url('./bg.jpeg')`, backgroundSize: 'cover' }}>
+    <div style={{ width: '100dvw', height: '100dvh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: `url('${baseURL}/bg.jpeg')`, backgroundSize: 'cover' }}>
       {
         isStart ? (
           <>
-            <div style={{ width: '340px', height: '480px', marginTop: 50 }}>
+            <div style={{ width: '340px', height: '480px', marginTop: 10 }}>
+              <div style={{ padding: 10, display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ color: '#FFFFFF', fontWeight: '400' }}>SCORE</div>
+                  <div style={{ marginLeft: 10, color: '#FFFFFF', fontWeight: '700' }}>{score}</div>
+                </div>
+                {
+                  isMute ? <GoMute size={20} fill={'#FFF'} onClick={() => setIsMute(false)} /> : <GoUnmute size={20} fill={'#FFF'} onClick={() => setIsMute(true)} />
+                }
+              </div>
               <div style={{ position: 'absolute', width: '340px', display: 'flex', justifyContent: 'center' }}>
-                <img src={`./${SUSHI[index]?.name}.png`} style={{ height: 50, opacity: 0.5 }} />
+                <img src={`${baseURL}/${SUSHI[index]?.name}.png`} style={{ height: 50, opacity: 0.5 }} />
               </div>
               <canvas style={{ width: '340px' }} ref={canvasRef} />
             </div>
 
             <div style={{ width: '340px', marginTop: 50 }}>
               <ul style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-                {SUSHI.map((v, i) => <li key={`${i}`} style={{ display: 'flex', padding: 5, opacity: `${i < 5 ? 1 : i > maxIndex ? 0.2 : 1}` }}><img src={`.//${v?.name}.png`} style={{ width: 25 }} /></li>)}
+                {SUSHI.map((v, i) => <li key={`${i}`} style={{ display: 'flex', padding: 5, opacity: `${i < 5 ? 1 : i > maxIndex ? 0.2 : 1}` }}><img src={`${baseURL}/${v?.name}.png`} style={{ width: 25 }} /></li>)}
               </ul>
             </div>
           </>
         ) : (
-          <div style={{ width: '100dvw', height: '100dvh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: `url('./bg.jpeg')`, backgroundSize: 'cover' }}>
-            <img src={'./salmon.png'} style={{ width: 200, marginBottom: 100 }} />
+          <div style={{ width: '100dvw', height: '100dvh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: `url('${baseURL}/bg.jpeg')`, backgroundSize: 'cover' }}>
+            <img src={`${baseURL}/salmon.png`} style={{ width: 200, marginBottom: 100 }} />
             <button style={{ fontSize: 24, color: '#FFFFFF', fontWeight: '700', cursor: 'pointer' }} onClick={() => setIsStart(true)}>Game Start</button>
           </div>
         )
