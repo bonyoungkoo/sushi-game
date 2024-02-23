3433cfc4aeeff9d4684935b297e66a879071ea1a
= <=>
2024-02-23 13:53:58 +0900

    homepage 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/sushi-game">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/main" element={<Main />} />
