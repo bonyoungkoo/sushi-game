9785d4cadabd4701dfe01d3d6d0d0df5f885c53d
= <=>
2024-02-23 16:02:19 +0900

    build base url 설정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sushi-game",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
 })
+
